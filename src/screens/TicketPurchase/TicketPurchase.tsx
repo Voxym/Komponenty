ec3f2c6a5e98ae63cfcd93afc663c65dad7775bf
Maciej Sobolewski <macieksob25@gmail.com>
2021-12-06 00:08:20 +0100

    ticket

--- src/screens/TicketPurchase/TicketPurchase.tsx
+++ src/screens/TicketPurchase/TicketPurchase.tsx
@@ -5,20 +5,24 @@ import { useState } from 'react';
 import { Formik, useFormik } from 'formik';
 import Button from '@mui/material/Button'
 import { margin } from '@mui/system';
-
-
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
+import { useInformation } from '../../commons/context';
 
 const options2 = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
+  { value: 'movie', label:  },
+  { value: 'room', label:  },
+  { value: 'date', label: },
+]
 
+
+interface Option {
+  
+      movieName: string;
+      roomNumber: number;
+      date: string;
+ 
+
+  
+}
 
 
 function TicketPurchase(){
@@ -28,17 +32,25 @@ function TicketPurchase(){
       console.log(value)
     }
   })
+
+ const {screens} = useInformation();
+  const [selectedOption,setSelectedOption]=useState(null)
+ const options:Option[] = []
+  options.map(o => {
+    o.movieName 
+  })
   return (
     <div className='Screen'>
      <h1>Purchase Ticket</h1>
       <form onSubmit={formik.handleSubmit}>
           <h3>Select movie from the list</h3>
-          <Select options={options}/>
+          
+          <Select options={options2} onChange={setSelectedOption(selectedOption)} value={selectedOption}/>
           
           <h3>Choose where do you want to sit</h3>
           <Select options={options2}/>
           <p></p>
-          <Button variant="contained" margin-top="10px">Contained</Button>
+          <Button variant="contained" margin-top="10px">Submit</Button>
       </form>
       </div>
   );
